46bdef0154dfa193ba483e16f310a25f702411c5
Matt Cox <mattcox27@gmail.com>
2026-07-09 22:04:27 +0100

    Loosens protocol requirements for BoundsEnumerable

--- Sources/Core/Protocols/BoundsEnumerable.swift
+++ Sources/Core/Protocols/BoundsEnumerable.swift
@@ -6,20 +6,27 @@
 //  Copyright © 2026 Matt Cox. All rights reserved.
 //
 
+import Cartesian
+
 /// A spatial structure that can enumerate the elements it contains that overlap
 /// a region of bounds.
 ///
 public protocol BoundsEnumerable {
+/// The type of Vector test against.
+///
+	associatedtype Vector: VectorProtocol
+
 /// The type of element contained within the structure.
 ///
-	associatedtype Element: Boundable
+	associatedtype Element
 
-/// Enumerate every element whose bounds overlap the provided bounds.
+/// Enumerate every element whose is partially contained in the provided
+/// bounds.
 ///
 /// - Parameters:
 ///   - bounds: The bounds to test elements against.
 ///   - perform: A closure invoked with each overlapping element. Return
 ///     `true` to continue, or `false` to stop enumeration.
 ///
-	func enumerate<T: Boundable>(bounds: T, _ perform: (Element) -> Bool) where T.Vector == Element.Vector
+	func enumerate<T: Boundable>(bounds: T, _ perform: (Element) -> Bool) where T.Vector == Vector
 }
